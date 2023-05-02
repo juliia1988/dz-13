27223d0c4107a4455f91570d1cd556a701594ea5
Yuliia Sokolova <65017585+juliia1988@users.noreply.github.com>
2023-05-02 17:21:45 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem planet1 = SolarSystem.MERCURY;
+        SolarSystem planet2 = SolarSystem.VENUS;
+        SolarSystem planet3 = SolarSystem.EARTH;
+        SolarSystem planet4 = SolarSystem.MARS;
+        SolarSystem planet5 = SolarSystem.JUPITER;
+        SolarSystem planet6 = SolarSystem.SATURN;
+        SolarSystem planet7 = SolarSystem.NEPTUNE;
+        SolarSystem planet8 = SolarSystem.NEPTUNE;
+
+
+        System.out.println("Планета: "+ planet3);
+        System.out.println("Порядковий номер: "+ planet3.getIdNumber());
+        System.out.println("Відстань від попередньої планети: "+ planet3.getSpaceFromPreviousPlanet() + " млн.км");
+        System.out.println("Радіус планети: "+ planet3.getRadius());
+        System.out.println("Наступна планета: " + planet3.getPreviousPlanet());
+        System.out.println("Наступна планета: " + planet3.getNextPlanet());
+        System.out.println("Відстань від Neptune до Сонця: "+ planet3.getSpaceFromSun() + " млн.км");
+    }
+
+
+}
---
+++ src/SolarSystem.java
@@ -0,0 +1,67 @@
+public enum SolarSystem {
+
+    MERCURY(1, 0, 2450, "No previous planet", "VENUS"),
+
+    VENUS(2, 50, 6050, "MERCURY", "EARTH"),
+    EARTH(3, 42, 6400, "VENUS", "MARS"),
+
+    MARS(4, 78, 3400, "EARTH", "JUPITER"),
+
+    JUPITER(5, 552, 71500, "MARS", "SATURN"),
+
+    SATURN(6, 620, 60000, "JUPITER", "URANUS"),
+
+    URANUS(7, 1500, 25500, "SATURN", "NEPTUNE"),
+
+    NEPTUNE(8, 1600, 24000, "URANUS", "No next planet");
+
+    private int idNumber;
+
+    SolarSystem(int idNumber, int spaceFromPreviousPlanet, int radius, String previousPlanet, String nextPlanet) {
+        this.idNumber = idNumber;
+        this.spaceFromPreviousPlanet = spaceFromPreviousPlanet;
+        this.radius = radius;
+        this.previousPlanet = previousPlanet;
+        this.nextPlanet = nextPlanet;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    private final int spaceFromPreviousPlanet;
+
+    public int getSpaceFromPreviousPlanet() {
+        return spaceFromPreviousPlanet;
+    }
+
+    private final int radius;
+
+    public int getRadius() {
+        return radius;
+    }
+
+    private String previousPlanet;
+
+    public String getPreviousPlanet() {
+        return previousPlanet;
+    }
+
+    private String nextPlanet;
+
+    public String getNextPlanet() {
+        return nextPlanet;
+    }
+
+    public int getSpaceFromSun() {
+        int spaceFromFirstPlanetToSun = 58;
+        int spaceFromSun = 0;
+
+        for (SolarSystem planet: SolarSystem.values()) {
+
+            spaceFromSun += planet.spaceFromPreviousPlanet + spaceFromFirstPlanetToSun;
+        }
+        return spaceFromSun;
+    }
+}
+
